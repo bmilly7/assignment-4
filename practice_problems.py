748da6a741b2741a02d341f1522a4358558636dc
bmilly7 <157335984+bmilly7@users.noreply.github.com>
2025-09-25 19:28:58 -0500

    completed all practice and timed challenge

--- practice_problems.py
+++ practice_problems.py
@@ -14,7 +14,16 @@ Output: False
 
 def has_duplicates(product_ids):
     # Your implementation here
-    pass
+    unique_ids = set(product_ids)
+    if len(unique_ids) == len(product_ids):
+        return False
+    else: 
+        return True
+
+#A set is a good structure choice for this problem because it ignores duplicate values. because we know this, we can compare the
+#set to the original list of numbers. If they are the same size, this means the set function did not remove any duplicates meaning there were not any duplicates
+#therefor we can return False. If the lengths are different, this means the set operator removed a duplicate value meaning we can return true
+    
 
 
 """
@@ -33,13 +42,26 @@ task_queue.remove_oldest_task() → "Email follow-up"
 class TaskQueue:
     def __init__(self):
         # Your initialization here
-        pass
+        self.tasks = []
 
     def add_task(self, task):
-        pass
+        self.tasks.append(task)
+
+
 
     def remove_oldest_task(self):
-        pass
+        if not self.task:
+            return None
+        else:
+            return self.task.pop(0)
+    
+
+    #this was my first attempt. I used a list but I think a queue may be faster. While appending to the end of a list is relatively fast O(1) time
+    # removing from the front of the list is slow O(n) because the other elements now have to be slid down in the array. 
+
+    # i beleive you should use a queue. but the built in structure of the file did not seem like we should be using a queue implemented with a linked list because there was not
+    #a node class. there was only a TaskQueue class.
+    #i beleive there is a deque built into python that would work. 
 
 
 """
@@ -57,10 +79,30 @@ tracker.get_unique_count() → 2
 
 class UniqueTracker:
     def __init__(self):
-        pass
+        self.integers = []
 
     def add(self, value):
-        pass
+       self.integers.append(value)
 
     def get_unique_count(self):
-        pass
+        self.unique_int = list(set(self.integers))
+        return len(self.unique_int)
+
+#fast verison
+
+'''
+
+class UniqueTracker:
+    def __init__(self):
+        self.unique_ints = set()
+
+    def add(self, value):
+        self.unique_ints.add(value)  # set ignores duplicates automatically
+
+    def get_unique_count(self):
+        return len(self.unique_ints)
+
+'''
+#a set is used because it already returns the unique values. Then we just count the length of the set for the total number
+#However my version is slow because I am storing them into a list every time  get_unique_count is called. this is slow 
+# you can convert directly into a set but I did not know you could do this. I put a faster version in comments. 
--- timed_challenge.py
+++ timed_challenge.py
@@ -1,3 +1,38 @@
 # Pick one question from timed_challenge.txt
 # Paste the question as a comment below
 # Set a timer for 30 minutes and complete the question!
+'''
+Group 2: Fast Lookup / Uniqueness
+5. Unique Word Count
+Count how many distinct words are in the collection.
+Input: "one fish two fish red fish blue fish"
+Output: 5
+'''
+string = "one fish two fish red fish blue fish green"
+def count_distict_words(string):
+    string2 = set(string.split())
+    return len(string2)
+
+
+
+
+total = count_distict_words(string)
+
+print(total)
+
+'''
+reflection (200 to 300 words) that answers the following:
+    - What structure you chose and why
+    - How the time limit shaped your decision 
+    - What trade-offs or compromises you made under time pressure
+
+I chose to use a set to solve this problem because it ask for uniqueness. Using a set seamed like the easiest and msot straight foward way
+to go about solving this problem. At first I tried to use a differnet data structure. I thought about using a dictionary for fast look ups to look up 
+each word to see if was unique. Then I decided that was the long way and a set was much easier since they already ignore duplicate values.
+This way we can split the string by the empty spaces and turn it into a set. All we have to do is count the length of the set and that should be the number of unique elements. 
+
+
+
+
+
+'''
